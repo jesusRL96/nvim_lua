2093a1432c9b70c690da94a74cafdbfe7c0afc1e
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-09-22 16:45:14 -0500

    firs commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require "editor.options"
+require "editor.keymaps"
+require "editor.plugins"
+require "editor.colorscheme"
+require "editor.cmp"
+require "editor.lsp"
+require "editor.telescope"
---
+++ lua/editor/options.lua
@@ -0,0 +1,17 @@
+o = vim.opt
+o.ignorecase = true
+o.autoindent = true
+o.number = true
+o.relativenumber = true
+o.showmatch = true
+o.hlsearch = true
+o.autoindent = true
+o.wildmode = {"longest","list"}
+o.mouse = "n"
+vim.cmd "filetype plugin indent on"
+o.tabstop = 4       
+o.expandtab = true     
+o.shiftwidth = 4  
+o.softtabstop = 4
+o.cursorline = true
+
