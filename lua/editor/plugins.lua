febaf9d8a63ab6da009d95780de8b36b4c9e3ddc
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-09-23 09:25:00 -0500

    treesitter

--- lua/editor/plugins.lua
+++ lua/editor/plugins.lua
@@ -62,6 +62,13 @@ return packer.startup(function(use)
   -- Telescope
   use "nvim-telescope/telescope.nvim"
   use 'nvim-telescope/telescope-media-files.nvim'
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
 
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
