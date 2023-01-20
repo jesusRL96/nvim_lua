fa35bb0e74b4a6292c1558a7499d6427955963c3
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2023-01-19 21:44:50 -0600

    Cambios en listchars/comments

--- lua/editor/options.lua
+++ lua/editor/options.lua
@@ -9,15 +9,23 @@ o.hlsearch = true
 o.wildmode = {"longest","list"}
 o.mouse = "a"
 vim.cmd "filetype plugin indent on"
-o.tabstop = 2       
-o.expandtab = false     
-o.shiftwidth = 2  
+o.tabstop = 2
+o.expandtab = false
+o.shiftwidth = 2
 o.softtabstop = 2
 --[[ o.cursorcolumn = true ]]
 o.cursorline = true
---[[ o.scrolloff = 999 ]]
+--[[ o.scro/home/jesus/.local/state/nvim/swap//%home%jesus%.config%nvim%lua%editor%opt
+ions.lua.swplloff = 999 ]]
 
 o.foldmethod = "expr"
 o.foldexpr = "nvim_treesitter#foldexpr()"
 o.foldlevel = 20
 o.foldlevelstart = -1
+o.listchars = {
+  tab = '»·',
+  extends = '⟩',
+  precedes = '⟨',
+  space = '·'
+}
+o.list = true
