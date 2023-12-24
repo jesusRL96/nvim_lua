f78fde70c024de5b79a182fb8ace7e62e63849cc
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2023-12-24 14:27:18 -0600

    excluir gitcommit de highlight

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -13,7 +13,7 @@ configs.setup({
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
+		disable = { "gitcommit" }, -- list of language that will be disabled
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml" } },
