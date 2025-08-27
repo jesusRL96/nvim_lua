b2d8a3134ec2d8f3eb404eaf18a608355abe4777
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2025-08-27 18:58:43 +0000

    keymaps toggleterm

--- lua/editor/keymaps.lua
+++ lua/editor/keymaps.lua
@@ -120,6 +120,22 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+-- Toggle term
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+
 -- Files
 keymap("n", "<leader>jsxf", "<cmd>set filetype=javascriptreact<cr>:LspRestart<cr>", term_opts)
 keymap("n", "<leader>jsf", "<cmd>set filetype=javascript<cr>:LspRestart<cr>", term_opts)
