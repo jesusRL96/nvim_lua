65f363d9e9999da43190595577cd28a0e6862219
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2025-10-14 23:16:08 +0000

    change colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "iibe/gruvbox-high-contrast" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-high-contrast",
+    },
+  }
+}
