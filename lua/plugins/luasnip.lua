ea225fa5a42ed0a0c71604f1c3292ebc1315138f
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2025-10-16 03:05:26 +0000

    add luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    opts = {
+      -- any options you want to override
+    },
+    config = function(_, opts)
+      -- First apply LazyVim's default config
+      require("luasnip").config.set_config(opts)
+      -- Then add your custom snippets
+      local snippets_path = vim.fn.stdpath("config") .. "/lua/snippets"
+      require("luasnip.loaders.from_lua").lazy_load({
+        paths = snippets_path
+      })
+    end,
+  },
+}
