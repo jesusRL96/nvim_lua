105347c757c7b3e666b9c52fe00aab372abe35df
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2025-11-09 19:21:55 +0000

    disable hover doc

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        -- Disable hover documentation
+        hover = {
+          enabled = false,
+        },
+        -- Disable signature help
+        signature = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
